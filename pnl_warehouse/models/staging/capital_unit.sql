0bcc5d0815d3a7bb114a76abccc31e441fb98fdd
JAMES.MCCONVILLE@BLUECRESTCAPITAL.COM <james.mcconville@bluecrestcapital.com>
2025-07-16 15:19:26 +0000

    FAAT-2362 Add strategy and start instrument type

--- pnl_warehouse/models/staging/capital_unit.sql
+++ pnl_warehouse/models/staging/capital_unit.sql
@@ -1,28 +1,28 @@
-with 
-capunit as (
-    select 
-        desk, 
-        capitalunit,
-        valuedate
-    from 
-        {{ source('pnl', 'transaction_value') }}
-),
-distinct_capunit as (
-    select 
-        desk,
-        capitalunit,
-        MIN(valuedate) as startdate
-    from
-        capunit
-    group by
-        desk,
-        capitalunit
-)
-select 
-    -- {{ dbt_utils.generate_series(upper_bound=1000000) }} as capunit_key, 
-    desk,
-    capitalunit,
-    startdate,
-    lag(startdate, 1, '2050-01-01') over (partition by desk, capitalunit order by startdate desc) endDate
-from
-    distinct_capunit;
+with capunit
+as (
+	select desk
+		,capitalunit
+		,valuedate
+	from {{ source('pnl', 'transaction_value') }}
+	)
+	,distinct_capunit
+as (
+	select desk
+		,capitalunit
+		,MIN(valuedate) as startdate
+	from capunit
+	group by desk
+		,capitalunit
+	)
+select {{ dbt_utils.generate_surrogate_key([
+            'desk', 
+        'capitalunit', 
+        'startdate']) }} as capunit_key
+	,desk
+	,capitalunit
+	,startdate
+	,cast(lag(startdate, 1, '2050-01-01') over (
+			partition by desk
+			,capitalunit order by startdate desc
+			) as datetime) endDate
+from distinct_capunit
